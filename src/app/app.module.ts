9d9411324308d2466b3fdaf4082c7e743cdf2ce9
SamShaqaryan <shaqaryan.sam@gmail.com>
2022-11-20 15:10:29 +0400

    feat(layouts,pages): add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AssortmentComponent } from './pages/assortment/assortment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AssortmentComponent
   ],
   imports: [
     BrowserModule,
